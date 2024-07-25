7bf8dc8932147685e25c6707a0ece5768c7e5b32
TatsHats <6642017@gmail.com>
2024-07-25 12:23:01 +0300

    feat: modified package-lock.json and src/index.js files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let numberStr = Math.abs(n).toString();
+    let result = '';
+    for (let i = numberStr.length - 1; i >= 0; i--) {
+        result += numberStr[i];
+    }
+    return Number(result);
 }
